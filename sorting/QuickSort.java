217820670d0f8f97e3a5302db44d11f16fe9dd4f
Harsh Singh <harshsin@usc.edu>
2015-01-27 21:26:38 -0800

    Created first version of quicksort without testing

---
+++ sorting/QuickSort.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+public class QuickSort {
+
+	public static int partition(Comparable[] a, int start, int end){
+		Comparable[] pivot = new Comparable[end];
+		int partitionIndex = start;
+
+		for (int i=0; i<end; i++ ) {
+			if (less(a[i],pivot)) {
+				exch(a[i],a[partitionIndex]);
+				partitionIndex++;
+			}
+		}
+		exch(a[partitionIndex],a[end]);
+		return partitionIndex;
+	}
+
+	public static void quickSort(Comparable[] a, int start, int end){
+ 
+		if (start<end) {
+			partitionIndex = partition(a, start, end)
+			quickSort(a, start, partitionIndex-1);
+			quickSort(a, partitionIndex+1, end)
+		}
+	}
+
+	public static sort(Comparable[] a){
+		int first = 0;
+		int last = a.length - 1;
+		quickSort(a, first, last);
+	}
+
+	public static boolean less(Comparable v, Comparable w){
+		return v.compareTo(w) < 0;
+	}
+
+	public static void exch(Comparable[] a, int i, int j){
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+	private static void show(Comparable[] a){
+		for (int i=0; i<a.length; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
+	}
+
+	private static boolean isSorted(Comparable[] a) {
+		for (int i = 1; i < a.length; i++)
+				if (less(a[i], a[i-1])) return false;
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		t = Integer.parseInt(in.nextLine());
+
+		Integer[] a = new Integer[t];
+		for (int i=0; i<t ; i++)
+			a[i] = in.nextInt();
+
+		sort(a);
+		assert isSorted(a);
+		show(a);	}
+
+	
+}
