2f4688cbfd9f4dfe29fa0d3ebb7d32394eb87bb6
Harsh Singh <harshsin@usc.edu>
2015-02-22 18:50:13 -0800

    adds the bag ADT

---
+++ datastruct/Bag.java
@@ -0,0 +1,76 @@
+
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Bag<Item> implements Iterable<Item> {
+		private int N;
+		private Node<Item> first;
+
+		private static class Node<Item> {
+				private Item item;
+				private Node<Item> next;
+		}
+
+		public Bag() {
+				first = null;
+				N = 0;
+		}
+
+		public boolean isEmpty() {
+				return first == null;
+		}
+
+		public int size() {
+				return N;
+		}
+
+		public void add(Item item) {
+				Node<Item> oldfirst = first;
+				first = new Node<Item>();
+				first.item = item;
+				first.next = oldfirst;
+				N++;
+		}
+
+		public Iterator<Item> iterator()  {
+				return new ListIterator<Item>(first);  
+		}
+
+		private class ListIterator<Item> implements Iterator<Item> {
+				private Node<Item> current;
+
+				public ListIterator(Node<Item> first) {
+						current = first;
+				}
+
+				public boolean hasNext()  { return current != null;                     }
+				public void remove()      { throw new UnsupportedOperationException();  }
+
+				public Item next() {
+						if (!hasNext()) throw new NoSuchElementException();
+						Item item = current.item;
+						current = current.next; 
+						return item;
+				}
+		}
+
+		public static void main(String[] args) {
+				Bag<String> bag = new Bag<String>();
+				Scanner in = new Scanner(System.in);
+				int T = Integer.parseInt(in.nextLine());
+				int i = 0;
+				while (i<T) {
+						String item = in.nextLine();
+						bag.add(item);
+						i++;
+				}
+
+				System.out.println("size of bag = " + bag.size());
+				for (String s : bag) {
+						System.out.println(s);
+				}
+		}
+
+
+}
