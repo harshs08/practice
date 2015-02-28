4f5d45915c5f73c17a406f913dd87b4093a3bffd
Harsh Singh <harshsin@usc.edu>
2015-02-28 13:53:11 -0800

    result link and problem description added

--- codility/PermCheck.rb
+++ codility/PermCheck.rb
@@ -1,3 +1,6 @@
+#https://codility.com/demo/results/demoZV4QMT-4SS/
+
+
 # you can use puts for debugging purposes, e.g.
 # puts "this is a debug message"
 
@@ -15,3 +18,38 @@ def solution(a)
     
     return 1
 end
+
+# A non-empty zero-indexed array A consisting of N integers is given.
+# A permutation is a sequence containing each element from 1 to N once, and only once.
+# For example, array A such that:
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# is a permutation, but array A such that:
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# is not a permutation, because value 2 is missing.
+# The goal is to check whether array A is a permutation.
+# Write a function:
+# def solution(a)
+# that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+# For example, given array A such that:
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# the function should return 1.
+# Given array A such that:
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# the function should return 0.
+# Assume that:
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [1..1,000,000,000].
+# Complexity:
+# expected worst-case time complexity is O(N);
+# expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+# Elements of input arrays can be modified.
