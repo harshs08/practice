5a451eb612d6ab4020866f71a8ac4cd2dd20135a
Harsh Singh <harshsin@usc.edu>
2015-03-04 15:49:41 -0800

    fixes the digit checks

--- miscel/PostFixEval.java
+++ miscel/PostFixEval.java
@@ -1,53 +1,69 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-public class PostFixEval{
+public class PostFixEval {
 
 	private char[] expression;
 
-	public PostFixEval(char[] expression){
+	public PostFixEval(char[] expression) {
 		this.expression = expression;
 	}
 
-	public boolean isDigit(char d){
-		if(d >= 0 && d<=9 ){ return true; }
-		else { false; }
+	public boolean isDigit(char d) {
+		if (d >= '0' && d <= '9') {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public boolean isOperator(char op){
-		if(op == "+" || op == "-" || op == "*" || op == "/")
+	public boolean isOperator(char op) {
+		if (op == '+' || op == '-' || op == '*' || op == '/')
 			return true;
+		
 		return false;
 	}
 
-	public int performOperation(char operator, int op1, int op2){
-		if(operator == "+"){
+	public int performOperation(char operator, int op1, int op2) {
+		if (operator == '+') {
 			return op1 + op2;
-		}
-		else if (operator == "-") {
+		} else if (operator == '-') {
 			return op1 - op2;
-		}
-		else if (operator == "*") {
+		} else if (operator == '*') {
 			return op1 * op2;
-		}
-		else if (operator == "/") {
+		} else if (operator == '/') {
 			return op1 / op2;
-		}
-		else{
+		} else {
 			return Integer.MAX_VALUE;
 		}
 	}
 
-	public int evaluatePostfix(){
-		int result = 0;
-		return result;
+	public int evaluatePostfix() {
+		Stack<Integer> S = new Stack<Integer>();
+		int res = 0;
+		int op1;
+		int op2;
+		for (char element : expression) {
+			if (isOperator(element)) {
+				op2 = S.pop();
+				op1 = S.pop();
+				res = performOperation(element, op1, op2);
+				S.push(res);
+			} else if(isDigit(element)) {
+				S.push(Character.getNumericValue(element));
+			}
+		}
+		return S.peek();
 	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		char[] exp = in.nextLine().toCharArray();
+
 		PostFixEval pf = new PostFixEval(exp);
 
 		int result = pf.evaluatePostfix();
-		System.out.println("The result of PostFix Evaluation: "+result);
+		System.out.println("Result: " + result);
+		in.close();
 	}
 }
