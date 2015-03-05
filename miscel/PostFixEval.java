322db09dccd3582ed26503aca92417295fe0b937
Harsh Singh <harshsin@usc.edu>
2015-03-04 16:53:17 -0800

    add the prefix evaluation also

--- miscel/PostFixEval.java
+++ miscel/PostFixEval.java
@@ -63,13 +63,50 @@ public class PostFixEval {
 		return S.peek();
 	}
 
+	public int evaluatePrefix() {
+		Stack<Integer> S = new Stack<Integer>();
+		int res = 0;
+		int op1;
+		int op2;
+		for (int i = expression.length-1; i>=0 ; i--) {
+			if(expression[i] == ' ' || expression[i] == ',') { continue; }
+			if (isOperator(expression[i])) {
+				op1 = S.pop();
+				op2 = S.pop();
+				res = performOperation(expression[i], op1, op2);
+				S.push(res);
+			} else if(isDigit(expression[i])) {
+				int operant = 0;
+				while(isDigit(expression[i]) && i>= 0){
+					operant = operant*10 + Character.getNumericValue(expression[i]); 
+					i--;
+				}
+				i++;
+				S.push(reverse(operant));
+			}
+		}
+		return S.peek();
+	}
+
+	public static int reverse(int n) {
+		int result = 0;
+		int rem;
+		while (n > 0) {
+			rem = n % 10;
+			n = n / 10;
+			result = result * 10 + rem;
+		}
+		return result;
+	 }
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		char[] exp = in.nextLine().toCharArray();
 
 		PostFixEval pf = new PostFixEval(exp);
 
-		int result = pf.evaluatePostfix();
+		//int result = pf.evaluatePostfix();
+		int result = pf.evaluatePrefix(); 
 		System.out.println("Result: " + result);
 		in.close();
 	}
