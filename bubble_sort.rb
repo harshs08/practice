83c1915129b404c508dc9fbbe4fad506e3307d15
Harsh Singh <harshsin@usc.edu>
2015-08-03 22:17:07 -0700

    solved bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+class BubbleSort
+  def sort(values)
+    i = 0
+    while i < values.length
+      j = 0
+      while j < values.length-1-i
+        if values[j+1] < values[j]
+          temp        = values[j+1]
+          values[j+1] = values[j]
+          values[j]   = temp
+        end
+        j = j+1
+      end
+      i = i+1
+    end
+    puts values
+  end
+end
+
+bs = BubbleSort.new
+arr = [3,5,1,2,4]
+bs.sort(arr)
